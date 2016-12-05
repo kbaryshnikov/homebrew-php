5c45a2aa68a28da7c5dd33352525418721173b16
Konstantin Baryshnikov <kbaryshnikov@upgate.co>
2016-12-05 20:55:42 +0300

    php71-msgpack

---
+++ Formula/php71-msgpack.rb
@@ -0,0 +1,23 @@
+require File.expand_path("../../Abstract/abstract-php-extension", __FILE__)
+
+class Php71Msgpack < AbstractPhp71Extension
+  init
+  desc "MessagePack serialization"
+  homepage "https://pecl.php.net/package/msgpack"
+  url "https://pecl.php.net/get/msgpack-2.0.1.tgz"
+  sha256 "d32aeef9af3be6135a06f29e28ec9f386cde9d90ad346a396d9ba8018a7044c6"
+  head "https://github.com/msgpack/msgpack-php.git"
+
+  def install
+    Dir.chdir "msgpack-#{version}" unless build.head?
+
+    ENV.universal_binary if build.universal?
+
+    safe_phpize
+    system "./configure", "--prefix=#{prefix}",
+                          phpconfig
+    system "make"
+    prefix.install "modules/msgpack.so"
+    write_config_file if build.with? "config-file"
+  end
+end
